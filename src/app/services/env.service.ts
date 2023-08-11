1184661ac68ca03a8010364ef48151f138ac1dba
Carloapf <carlo.pano@uabc.edu.mx>
2023-08-11 11:51:55 -0700

    Primer commit

--- src/app/services/env.service.ts
+++ src/app/services/env.service.ts
@@ -4,11 +4,14 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class EnvService {
+  //API_URL = 'http://dfcmurua.duranfreight.net:81/duranApi/index.php/choferesWeb/';
 
   API_URL = 'http://localhost/duranApi/index.php/choferes/';
   API_URL_DIESEL = 'http://localhost/duranApi/index.php/diesel/';
+  //API_URL_DIESEL = 'http://dfcmurua.duranfreight.net:81/duranApi/index.php/diesel/';
   // 'http://localhost:8083/API/index.php/api/'; // 'http://erpbajapk.dyndns.org/bajapk/API/index.php/api/';
   HARVEST_URL = 'http://localhost/duranApi/';
+  //HARVEST_URL = 'http://dfcmurua.duranfreight.net:81/duranApi/';
   Version = '1.0.6';
 
   constructor() { }
